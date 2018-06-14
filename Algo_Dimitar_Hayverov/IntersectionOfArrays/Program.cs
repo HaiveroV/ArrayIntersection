5ab30449ce381e50e29fbdfe18c62de93d730ca7
HaiveroV <mitkohaiverov@abv.bg>
2018-06-14 17:40:39 +0300

    Initial commit

---
+++ Algo_Dimitar_Hayverov/IntersectionOfArrays/Program.cs
@@ -0,0 +1,59 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace IntersectionOfArrays
+    {
+        class Program
+        {
+            static void Main(string [] args )
+            {
+                Console.Write("Enter the size of the first array : ");
+
+                int n = int.Parse(Console.ReadLine());
+                int[] array1 = new int[n];
+
+                Console.WriteLine("");
+                Console.WriteLine("***************************************");
+                Console.WriteLine("");
+                Console.Write("Enter the size of the second array : ");
+
+                int m = int.Parse(Console.ReadLine());
+                int[] array2 = new int[m];
+
+                Console.WriteLine("");
+                Console.WriteLine("***************************************");
+                Console.WriteLine("");
+
+                for (int i = 0; i < n; i++)
+                {
+                Console.Write("Enter the values for the first array : ");
+                array1[i] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("***************************************");
+                Console.WriteLine("");
+
+                for (int j = 0; j < m; j++)
+                {
+                Console.Write("Enter the values for the second array : ");
+                array2[j] = int.Parse(Console.ReadLine());
+                }
+
+                
+                Console.WriteLine("***************************************");
+                // Call Intersect extension method.
+                var intersect = array1.Intersect(array2);
+                // Write intersection to screen.
+                foreach (int value in intersect)
+                {
+                Console.WriteLine("The intersection of the two arrays are these numbers:" + value);
+                }
+
+        }
+
+        }
+    }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ArrayIntersection
+
 Showing a way to intersect 2 arrays
+
+* First I declare two arrays of type int, which can take numbers from console
+as user input.
+
+* Second I need to parse the string into an integer for the name of the variable
+which holds the size of the array that the user declares.
+
+* Third I use for loop to input the desired numbers in the array.
+
+* Fourth I use the Intersect extension method to find the common numbers
+between the two arrays.
+
+* Fifth I use foreach loop to print the intersection of the two arrays.
